0db47778a3a97387cabac6a176da08873e29e4d2
Emmett Butler <emmett.butler321@gmail.com>
2013-11-03 13:35:43 -0500

    test success and fail logins

--- server/test.py
+++ server/test.py
@@ -38,15 +38,25 @@ class TestServer(tornado.testing.AsyncHTTPTestCase):
         )
         self.wait()
 
+    def _test_login(self, pw, callback):
+        def handle(response):
+            callback(response)
+            self.stop()
+        self.http_client.fetch(
+            self.get_url("/login?") + urllib.urlencode(
+                {'email': 'emmett.butler612@gmail.co', 'pw': pw}
+            ),
+            handle
+        )
+        self.wait()
+
     def test_login(self):
         def handle_login(response):
             assert response.code == 200, "Login should return a 200"
             assert 'access_token' in json.loads(response.body)['data'], "Login should return an access token"
-            self.stop()
-        self.http_client.fetch(
-            self.get_url("/login?") + urllib.urlencode(
-                {'email': 'emmett.butler612@gmail.co', 'pw': 'buts'}
-            ),
-            handle_login
-        )
-        self.wait()
+        self._test_login("butts", handle_login)
+
+    def test_bad_login(self):
+        def handle_login(response):
+            assert response.code == 403, "Bad login should return a 403"
+        self._test_login("wrong", handle_login)
