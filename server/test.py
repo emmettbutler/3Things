c259b96f893b915049b11c44565bd02451c8c3ad
Emmett Butler <emmett.butler321@gmail.com>
2013-11-03 13:30:54 -0500

    add more tests

--- server/test.py
+++ server/test.py
@@ -1,4 +1,6 @@
 import unittest
+import urllib
+import json
 
 import tornado.testing
 import tornado.httpclient
@@ -13,9 +15,38 @@ class TestServer(tornado.testing.AsyncHTTPTestCase):
     def setUp(self):
         super(TestServer, self).setUp()
 
-    def test_register(self):
+    def test_register_no_args(self):
         def handle_no_args(response):
             assert response.code == 400
             self.stop()
         self.http_client.fetch(self.get_url("/register"), handle_no_args)
+        self.wait()
+
+    def test_new_user(self):
+        identifier = "emmett.butler321@gmail.com"
+        def handle_new_user(response):
+            assert response.code == 201
+            app = self.get_app()
+            db = app.dbclient.three_things
+            db.users.remove({'email': identifier})
+            self.stop()
+        params = {'identifier': identifier,
+                  'name': "emmett butler", "pw": "test", "pwc": "test"}
+        self.http_client.fetch(
+            self.get_url("/register?" + urllib.urlencode(params)),
+            handle_new_user
+        )
+        self.wait()
+
+    def test_login(self):
+        def handle_login(response):
+            assert response.code == 200, "Login should return a 200"
+            assert 'access_token' in json.loads(response.body)['data'], "Login should return an access token"
+            self.stop()
+        self.http_client.fetch(
+            self.get_url("/login?") + urllib.urlencode(
+                {'email': 'emmett.butler612@gmail.co', 'pw': 'butts'}
+            ),
+            handle_login
+        )
         self.wait()
