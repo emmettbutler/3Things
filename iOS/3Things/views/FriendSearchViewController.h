43d5abc6f638a3f0ee9e72001b11cd6ed7e55e2e
Emmett Butler <emmett.butler321@gmail.com>
2013-11-13 08:27:50 -0500

    init friend search screen

---
+++ iOS/3Things/views/FriendSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendSearchViewController.h
+//  3Things
+//
+//  Created by Emmett Butler on 11/13/13.
+//  Copyright (c) 2013 Emmett Butler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendSearchViewController : UIViewController
+
+@end
---
+++ iOS/3Things/views/FriendSearchViewController.m
@@ -0,0 +1,38 @@
+//
+//  FriendSearchViewController.m
+//  3Things
+//
+//  Created by Emmett Butler on 11/13/13.
+//  Copyright (c) 2013 Emmett Butler. All rights reserved.
+//
+
+#import "FriendSearchViewController.h"
+
+@interface FriendSearchViewController ()
+
+@end
+
+@implementation FriendSearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
