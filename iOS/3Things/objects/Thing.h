df962426df776412d4f158fb343883588527af77
Emmett Butler <emmett.butler321@gmail.com>
2013-12-27 20:05:33 -0500

    revert bad Thing change

--- iOS/3Things/objects/Thing.h
+++ iOS/3Things/objects/Thing.h
@@ -2,18 +2,21 @@
 //  Thing.h
 //  3Things
 //
-//  Created by Emmett Butler on 10/6/13.
+//  Created by Emmett Butler on 11/6/13.
 //  Copyright (c) 2013 Emmett Butler. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
+@class ShareDay;
 
 @interface Thing : NSManagedObject
 
 @property (nonatomic, retain) NSDate * date;
-@property (nonatomic, retain) NSString * text;
 @property (nonatomic, retain) NSString * localImageURL;
+@property (nonatomic, retain) NSString * text;
+@property (nonatomic, retain) NSNumber * index;
+@property (nonatomic, retain) ShareDay *day;
 
 @end
