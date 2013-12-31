494d72c8e6cc4c8559b36be0bd863f8d94357d77
Emmett Butler <emmett.butler321@gmail.com>
2013-12-30 19:14:40 -0500

    styling for image prompt

--- iOS/3Things/views/PhotoPromptViewController.m
+++ iOS/3Things/views/PhotoPromptViewController.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2013 Emmett Butler. All rights reserved.
 //
 
+#import "TTNetManager.h"
 #import "PhotoPromptViewController.h"
 #import "EditThingViewController.h"
 #import "AssetsLibrary/AssetsLibrary.h"
@@ -22,7 +23,7 @@
     [super viewDidLoad];
 	TTLog(@"Photo prompt loaded");
     
-    self.view.backgroundColor = [UIColor colorWithRed:.3 green:.3 blue:.3 alpha:1];
+    self.view.backgroundColor = [UIColor colorWithWhite:1 alpha:1];
     CGRect screenFrame = CGRectMake(self.view.bounds.origin.x, self.view.bounds.origin.y, self.view.bounds.size.width, self.view.bounds.size.height-20);
     CGRect popupFrame = CGRectMake(0, screenFrame.size.height-180, screenFrame.size.width, 200);
     
@@ -30,25 +31,31 @@
     [takeButton addTarget:self
                    action:@selector(takeWasTouched)
          forControlEvents:UIControlEventTouchDown];
-    [takeButton setTitle:@"Take Photo" forState:UIControlStateNormal];
+    [takeButton setTitle:@"TAKE PHOTO" forState:UIControlStateNormal];
     takeButton.frame = CGRectMake(0, 0, popupFrame.size.width, 60);
+    takeButton.titleLabel.font = [UIFont fontWithName:HEADER_FONT size:12];
     [self.view addSubview:takeButton];
+    [takeButton setTitleColor:[[TTNetManager sharedInstance] colorWithHexString:BUTTON_TEXT_BLUE_COLOR] forState:UIControlStateNormal];
     
     UIButton *chooseButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     [chooseButton addTarget:self
                    action:@selector(chooseWasTouched)
          forControlEvents:UIControlEventTouchDown];
-    [chooseButton setTitle:@"Choose from Photos" forState:UIControlStateNormal];
+    [chooseButton setTitle:@"CHOOSE FROM PHOTOS" forState:UIControlStateNormal];
     chooseButton.frame = CGRectMake(0, 60, popupFrame.size.width, 60);
+    chooseButton.titleLabel.font = [UIFont fontWithName:HEADER_FONT size:12];
     [self.view addSubview:chooseButton];
+    [chooseButton setTitleColor:[[TTNetManager sharedInstance] colorWithHexString:BUTTON_TEXT_BLUE_COLOR] forState:UIControlStateNormal];
     
     UIButton *cancelButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     [cancelButton addTarget:self
                      action:@selector(cancelWasTouched)
            forControlEvents:UIControlEventTouchDown];
-    [cancelButton setTitle:@"Cancel" forState:UIControlStateNormal];
+    [cancelButton setTitle:@"CANCEL" forState:UIControlStateNormal];
     cancelButton.frame = CGRectMake(0, 140, popupFrame.size.width, 60);
+    cancelButton.titleLabel.font = [UIFont fontWithName:HEADER_FONT size:12];
     [self.view addSubview:cancelButton];
+    [cancelButton setTitleColor:[[TTNetManager sharedInstance] colorWithHexString:BUTTON_TEXT_BLUE_COLOR] forState:UIControlStateNormal];
 }
 
 - (void)takeWasTouched
