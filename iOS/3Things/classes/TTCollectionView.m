1c401d04788d1a3fe53737fc18eb686c705a4816
Emmett Butler <emmett.butler321@gmail.com>
2014-01-14 08:54:11 -0500

    document the horribleness

--- iOS/3Things/classes/TTCollectionView.m
+++ iOS/3Things/classes/TTCollectionView.m
@@ -11,6 +11,19 @@
 
 @implementation TTCollectionView
 
+/*
+ * WARNING
+ * 
+ * THIS MEANS YOU
+ *
+ * If you use this class, every single touch event that hits your instance will be sent to this method
+ * This class is here only to manually handle touch events that are meant for collectionViews that are
+ * nested inside of the friend feed tableview. It's highly unlikely that this class will work with any
+ * other nesting of views.
+ *
+ * You probably should not use this class.
+ */
+
 -(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
     /*
@@ -22,19 +35,24 @@
     UIView *view;
     if (((UIView *)subviews[0]).tag == FLAG_TAG) {
         TTLog(@"Touch view first subview is flag");
-        // this view is not inside a tableview
+        // this view is not inside a tableview - ie this touch did not originate on a tableviewcell
         view = touch.view.superview.superview;
     } else {
+        // this path assumes a very specific nesting of views containing the collectionView
+        // if this view hierarchy changes, this needs to as well.
         UIView *secondSubview = firstSubview.subviews[0];
         view = secondSubview.subviews[2];
     }
+    // if this condition is ever not true then the whole assumption of the above lines is wrong
+    // by this point, view should be the TTCollectionView
+    assert([view isKindOfClass:[TTCollectionView class]]);
     if ([view isKindOfClass:[TTCollectionView class]]) {
-        TTLog(@"View is a TTCollectionView");
         TTCollectionView *collection = (TTCollectionView*)view;
         NSArray *subviews = collection.subviews;
         int i;
         for (i = 0; i < 3; i++) {
-            if (CGRectContainsPoint(((UIView*)subviews[i]).frame, [touch locationInView:((UIView*)subviews[0]).superview])) {
+            // determine the indexPath by getting the touch's location in the collectionview
+            if (CGRectContainsPoint(((UIView*)subviews[i]).frame, [touch locationInView:collection])) {
                 TTLog(@"Touched collectionview index %d", i);
                 NSIndexPath *indexPath = [NSIndexPath indexPathForRow:i inSection:0];
                 [collection.delegate collectionView:collection didSelectItemAtIndexPath:indexPath];
