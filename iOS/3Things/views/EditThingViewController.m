c5926530c845473781b7d14b40305e749b240c01
Emmett Butler <emmett.butler321@gmail.com>
2014-01-14 11:58:32 -0500

    fix camera button scale

--- iOS/3Things/views/EditThingViewController.m
+++ iOS/3Things/views/EditThingViewController.m
@@ -98,9 +98,10 @@
     [imgButton addTarget:self
                action:@selector(imgButtonWasTouched)
      forControlEvents:UIControlEventTouchDown];
-    imgButton.frame = CGRectMake(screenFrame.size.width*.05, buttonY, 40, buttonHeight);
+    imgButton.frame = CGRectMake(screenFrame.size.width*.05, buttonY, 45, buttonHeight);
     imgButton.backgroundColor = [[TTNetManager sharedInstance] colorWithHexString:BUTTON_COLOR];
     [imgButton setBackgroundImage:[UIImage imageNamed:@"Camera.png"] forState:UIControlStateNormal];
+    [imgButton setContentMode:UIViewContentModeScaleAspectFit];
     imgButton.layer.cornerRadius = BUTTON_CORNER_RADIUS;
     [self.view addSubview:imgButton];
     
